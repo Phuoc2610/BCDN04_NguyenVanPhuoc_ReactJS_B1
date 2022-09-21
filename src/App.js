bcfd2c2e15cf69199665081d5e36a47cb6f95c74
Phuoc <phuocnvgcd191236@fpt.edu.vn>
2022-09-21 20:33:46 +0700

    project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './BaiTapComponent/Header';
+import Body from './BaiTapComponent/Body';
+import Footer from './BaiTapComponent/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
